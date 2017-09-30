c5437f87f92448790adf25be2076755bf3543fb5
Rohan Thorat <rthorat1@binghamton.edu>
2017-09-30 13:08:10 -0400

    creating recipe model-shopping component-recipe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'we are on for testing', 'https://cdn.pixabay.com/photo/2017/07/02/12/10/salad-2464296_1280.jpg'),
+    new Recipe('A test recipe', 'we are on for testing', 'https://cdn.pixabay.com/photo/2017/07/02/12/10/salad-2464296_1280.jpg')
+
+  ];
+
 
   constructor() { }
 
