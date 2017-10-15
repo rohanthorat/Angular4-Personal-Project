59bb05ce383ce5e6cea90f720cd9738646b7d3c4
Rohan Thorat <rthorat1@binghamton.edu>
2017-10-14 20:59:16 -0400

    forms- adding shopping list form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,7 +8,7 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe',
                'This is simply a test',
-               'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+               'http://www.gharanarestaurant.com/wp-content/uploads/2014/06/north-indian-1024x549.jpg',
                 [
                   new Ingredient('Meat',10),
                   new Ingredient('French Fries',20)
@@ -16,7 +16,7 @@ export class RecipeService {
 
     new Recipe('Another Test Recipe',
                'This is simply a test',
-               'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+               'http://www.itsgoa.com/wp-content/uploads/2016/07/28vada-pav-1.jpg',
                 [
                   new Ingredient('Buns',10),
                   new Ingredient('Meat',30),
